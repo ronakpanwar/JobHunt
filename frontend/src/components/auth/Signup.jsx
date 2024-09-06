2acfe933aac615e4b474b573e4b30607b7ea429d
Ronak panwar <127338535+ronakpanwar@users.noreply.github.com>
2024-09-06 17:36:21 +0530

    Update Signup.jsx

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -51,7 +51,7 @@ const Signup = () => {
     }
     try {
       dispatch(setLoading(true))
-         const res = await axios.post(`https://job-hunt-topaz-omega.vercel.app/api/user/register` , formData ,{
+         const res = await axios.post(`https://job-hunt-api.vercel.app/api/user/register` , formData ,{
           headers:{
             "Content-Type":"multipart/form-data"
           },
