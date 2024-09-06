efbe61bbe3611a2a8793ee38d4182bd2e1c3d993
Ronak panwar <127338535+ronakpanwar@users.noreply.github.com>
2024-09-06 19:47:12 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -5,7 +5,7 @@ const connectDB = async()=>{
         await mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000, // Adjust the timeout as needed
+    serverSelectionTimeoutMS: 10000, // Adjust the timeout as needed
   });
         console.log("mongodb is connected");
     } catch (error) {
